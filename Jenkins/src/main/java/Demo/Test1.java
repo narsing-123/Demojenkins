b9710c047bddd95dbedf19ce8cb05c54181dfc9d
Narsing kosiki <Narsing kosiki@LAPTOP-2S3C4UNF>
2023-01-09 16:13:45 +0530

    first commit

---
+++ Jenkins/src/main/java/Demo/Test1.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+import org.testng.Reporter;
+
+
+public class Test1 {
+	
+	@Test
+	public void m1() {
+		Reporter.log("Hello",true);
+	}
+
+}
---
+++ Jenkins/src/main/java/Demo/Test2.java
@@ -0,0 +1,16 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+import org.testng.Reporter;
+
+
+public class Test2 {
+	
+
+	@Test
+	public void m2() {
+		Reporter.log("Hii",true);
+	}
+
+}
